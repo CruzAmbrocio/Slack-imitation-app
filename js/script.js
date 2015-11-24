ed82a9c313ae57e3266755b5a4dd2f727506f825
CruzAmbrocio <cruzambrocio1409@gmail.com>
2015-11-24 00:41:42 -0600

    Begins design of first screen

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-//Js code
+//Js code	
+$(document).ready(function(){
+   // jQuery methods go here...
+    $(".forCreate").hide();
+    $("#createAccount").click(function(){
+        $(".forLog").hide();
+        $(".forCreate").show();
+    });
+    $("#ReturnLog").click(function() {
+        $(".forCreate").hide();
+        $(".forLog").show();
+    })
+});
