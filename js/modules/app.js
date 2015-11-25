42908533b63e9b51fffc2301d628aa74ac2b64b6
CruzAmbrocio <cruzambrocio1409@gmail.com>
2015-11-24 22:57:25 -0600

    end desing of login and add parts of Firebase

--- js/modules/app.js
+++ js/modules/app.js
@@ -1,1 +1,12 @@
 //Angular Js code
+var app=angular.module('myApp', ['']);
+  app.controller('createAccount1', function($scope) {
+    $scope.data=function () {
+        // body...
+        var Name=document.getElementById("txtName").value;
+        var email=document.getElementById("txtEmail").value;
+        var pass=document.getElementById("txtPass").value;
+        var pass2=document.getElementById("txtPass2").value;
+        console.log(Name, email, pass2, pass)
+    };
+  });
--- js/script.js
+++ js/script.js
@@ -1,13 +1,35 @@
 //Js code	
-$(document).ready(function(){
+$(document).ready(function() {
    // jQuery methods go here...
     $(".forCreate").hide();
-    $("#createAccount").click(function(){
+    $(".forReset").hide();
+
+    $("#createAccount").click(function() {
         $(".forLog").hide();
         $(".forCreate").show();
     });
     $("#ReturnLog").click(function() {
         $(".forCreate").hide();
         $(".forLog").show();
-    })
+    });
+    $("#resetPass").click(function() {
+        $(".forLog").hide();
+        $(".forReset").show();
+    });
+    $("#cancelReset").click(function() {
+        $(".forReset").hide();
+        $(".forLog").show();
+    });
+});
+
+var ref = new Firebase("https://loginappfrist.firebaseio.com/");
+ref.createUser({
+  email    : "bobtony@firebase.com",
+  password : "correcthorsebatterystaple"
+}, function(error, userData) {
+  if (error) {
+    console.log("Error creating user:", error);
+  } else {
+    console.log("Successfully created user account with uid:", userData.uid);
+  }
 });
